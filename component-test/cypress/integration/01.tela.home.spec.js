9ffca976a6e5733f2c81296e00f558b7235118a2
Carlos Monteiro <carlosm@ciandt.com>
2021-04-12 19:46:30 -0300

    configured the component test in the pipeline

--- component-test/cypress/integration/01.tela.home.spec.js
+++ component-test/cypress/integration/01.tela.home.spec.js
@@ -1,5 +1,5 @@
 describe('teste da tela inicial', () => {
-    it('verificando o conteuda ta tela', () => {
+    it('verificando o conteudo ta tela', () => {
         cy.visit('http://localhost:3000/')
         cy.get('h1').contains('Bem vindo!')
         cy.get('h2').contains('Visitante')
